14681d822f99d010b245118094d073ebdb3e2c35
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2024-08-26 19:15:44 +0900

    Delete h_11주차-TS-프로젝트 directory

--- h_11주차-TS-프로젝트/src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'https://restcountries.com',
-});
-
-export default axiosInstance;
--- h_11주차-TS-프로젝트/src/api/countryApi.tsx
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "./api.js";
-import {CountryApiResponse} from "../interface/countryInterface.tsx";
-
-export const getCountries = async (): Promise<CountryApiResponse[]> => {
-  const axiosInstance1 = await axiosInstance.get("/v3.1/all");
-  return axiosInstance1.data.slice(0, 40);
-}
