b2684f7deb26c353af8a45623f9643661a9ce975
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2024-08-26 19:15:31 +0900

    Delete g_10주차-아웃소싱팀프로젝트 directory

--- g_10주차-아웃소싱팀프로젝트/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from 'react-router-dom';
-import Router from './shared/Router';
-
-const App = () => {
-  return <RouterProvider router={Router} />;
-};
-
-export default App;
